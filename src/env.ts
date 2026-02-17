416731d9b88ed9717e09a31ba06141058a808cdc
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2026-02-18 03:03:08 +0600

    fix env bug

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,13 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const env = createEnv({
-  server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
-  },
-  client: {
-    NEXT_PUBLIC_API_URL: z.string().url(),
-    NEXT_PUBLIC_IMGBB_API_KEY: z.string(),
-  },
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_IMGBB_API_KEY: process.env.NEXT_PUBLIC_IMGBB_API_KEY,
-  },
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]),
+  NEXT_PUBLIC_API_URL: z.string().url(),
+  NEXT_PUBLIC_IMGBB_API_KEY: z.string(),
+});
+
+export const env = envSchema.parse({
+  NODE_ENV: process.env.NODE_ENV,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_IMGBB_API_KEY: process.env.NEXT_PUBLIC_IMGBB_API_KEY,
 });
