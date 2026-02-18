3d0b2b1afd069fa651e275b3304de2dfcdf8950a
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2026-02-18 23:17:26 +0600

    fix deploy error

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,21 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["development", "test", "production"]),
+  NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+
   NEXT_PUBLIC_API_URL: z.string().url(),
   NEXT_PUBLIC_IMGBB_API_KEY: z.string(),
 });
 
-export const env = envSchema.parse({
+const result = envSchema.safeParse({
   NODE_ENV: process.env.NODE_ENV,
   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   NEXT_PUBLIC_IMGBB_API_KEY: process.env.NEXT_PUBLIC_IMGBB_API_KEY,
 });
+
+if (!result.success) {
+  console.error("❌ Invalid environment variables", result.error.flatten());
+  throw new Error("Invalid environment variables");
+}
+
+export const env = result.data;
