0984d3880ead5357f35c0a6b8dec391cffc834f7
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2026-02-19 14:59:03 +0600

    fix env problem in deploy

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  baseURL: "https://pharmetix-client.vercel.app",
   plugins: [
     inferAdditionalFields({
       user: {
