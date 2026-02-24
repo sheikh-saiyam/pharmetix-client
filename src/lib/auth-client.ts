b5ce4e9af1e5172df2b4056cebac2e1e1fedfb91
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2026-02-24 23:36:06 +0600

    please fix problem!

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://pharmetix-client.vercel.app",
   fetchOptions: {
     credentials: "include",
   },
